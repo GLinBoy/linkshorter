1c8bc92e96bc9dcc6d2ed12a2d90f3f6488b07ea
GLinBoy <hojjatabedie@gmail.com>
2020-11-22 14:38:50 +0330

    config properties

---
+++ src/main/java/com/glibnoy/linkshorter/util/ApplicationProperties.java
@@ -0,0 +1,27 @@
+package com.glibnoy.linkshorter.util;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Properties specific to Test.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+@Getter
+@Setter
+public final class ApplicationProperties {
+
+	private final Config config = new Config();
+	
+	@Getter
+	@Setter
+	public static final class Config {
+		private int codeSize;
+		private String hashAlgorithm;
+	}
+}
