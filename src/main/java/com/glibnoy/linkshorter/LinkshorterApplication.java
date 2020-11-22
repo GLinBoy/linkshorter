14f05f305111efc29fb971ac4ad4b5ab685b1dad
GLinBoy <hojjatabedie@gmail.com>
2020-11-22 14:39:39 +0330

    enable config properties and JPA auditing

--- src/main/java/com/glibnoy/linkshorter/LinkshorterApplication.java
+++ src/main/java/com/glibnoy/linkshorter/LinkshorterApplication.java
@@ -2,8 +2,14 @@ package com.glibnoy.linkshorter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.glibnoy.linkshorter.util.ApplicationProperties;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableConfigurationProperties({ ApplicationProperties.class })
 public class LinkshorterApplication {
 
 	public static void main(String[] args) {
