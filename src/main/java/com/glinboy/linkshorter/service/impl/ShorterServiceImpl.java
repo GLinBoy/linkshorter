f5ef656c00d7a7a2536f5f8a9fa9a8be4480ea44
GLinBoy <hojjatabedie@gmail.com>
2021-10-19 20:32:01 +0330

    👌 IMPROVE: Format code

--- src/main/java/com/glinboy/linkshorter/service/impl/ShorterServiceImpl.java
+++ src/main/java/com/glinboy/linkshorter/service/impl/ShorterServiceImpl.java
@@ -4,6 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,54 +15,56 @@ import com.glinboy.linkshorter.service.ShorterServiceApi;
 import com.glinboy.linkshorter.service.UrlEncoderApi;
 import com.glinboy.linkshorter.service.dto.UrlDTO;
 import com.glinboy.linkshorter.util.ApplicationProperties;
+import com.sun.jdi.InternalException;
 
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
 public class ShorterServiceImpl implements ShorterServiceApi {
-	
-	private final UrlRepository repository;
-	
-	private final ModelMapper mapper;
-	
-	private final UrlEncoderApi urlEncoder;
-	
-	private final ApplicationProperties applicationProperties;
 
-	public ShorterServiceImpl(UrlRepository repository,
-			UrlEncoderApi urlEncoder,
-			ModelMapper mapper,
-			ApplicationProperties applicationProperties) {
-		this.repository = repository;
-		this.mapper = mapper;
-		this.urlEncoder = urlEncoder;
-		this.applicationProperties = applicationProperties;
-	}
+  private final UrlRepository repository;
 
-	@Override
-	public UrlDTO makeShort(UrlDTO urlDTO) {
-		log.info("URL requested to become shoter: {}", urlDTO);
-		Url url = mapper.map(encodeUrl(urlDTO), Url.class);
-		url = repository.save(url);
-		return mapper.map(url, UrlDTO.class);
-	}
+  private final ModelMapper mapper;
 
-	@Override
-	public Optional<UrlDTO> getShort(String code) {
-		return repository.findOneByCode(code)
-				.map(c -> mapper.map(c, UrlDTO.class));
-	}
-	
-	private UrlDTO encodeUrl(UrlDTO url) {
-		try {
-			String encode = urlEncoder.encode(url.getOriginal());
-			url.setCode(encode.substring(0, applicationProperties.getConfig().getCodeSize()));
-			return url;
-		} catch (NoSuchAlgorithmException e) {
-			log.error(e);
-			throw new ResponseStatusException(HttpStatus.FAILED_DEPENDENCY);
-		}
-	}
+  private final UrlEncoderApi urlEncoder;
+
+  private final ApplicationProperties applicationProperties;
+
+  public ShorterServiceImpl(UrlRepository repository, UrlEncoderApi urlEncoder, ModelMapper mapper,
+      ApplicationProperties applicationProperties) {
+    this.repository = repository;
+    this.mapper = mapper;
+    this.urlEncoder = urlEncoder;
+    this.applicationProperties = applicationProperties;
+  }
+
+  @Override
+  public UrlDTO makeShort(UrlDTO urlDTO) {
+    log.info("URL requested to become shoter: {}", urlDTO);
+    Url url = mapper.map(encodeUrl(urlDTO), Url.class);
+    try {
+      url = repository.save(url);
+    } catch (DataIntegrityViolationException ex) {
+      throw new InternalException(ex.getMostSpecificCause().getMessage());
+    }
+    return mapper.map(url, UrlDTO.class);
+  }
+
+  @Override
+  public Optional<UrlDTO> getShort(String code) {
+    return repository.findOneByCode(code).map(c -> mapper.map(c, UrlDTO.class));
+  }
+
+  private UrlDTO encodeUrl(UrlDTO url) {
+    try {
+      String encode = urlEncoder.encode(url.getOriginal());
+      url.setCode(encode.substring(0, applicationProperties.getConfig().getCodeSize()));
+      return url;
+    } catch (NoSuchAlgorithmException e) {
+      log.error(e);
+      throw new ResponseStatusException(HttpStatus.FAILED_DEPENDENCY);
+    }
+  }
 
 }
