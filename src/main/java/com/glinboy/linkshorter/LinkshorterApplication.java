6ec2efb33b4d816e7ccbb46ecd7d49ad16e38c45
GLinBoy <hojjatabedie@gmail.com>
2021-10-19 20:29:34 +0330

    🐛 FIX: Exclude ErrorMvcAutoConfiguration for configuration problem

--- src/main/java/com/glinboy/linkshorter/LinkshorterApplication.java
+++ src/main/java/com/glinboy/linkshorter/LinkshorterApplication.java
@@ -1,7 +1,9 @@
 package com.glinboy.linkshorter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -10,6 +12,7 @@ import com.glinboy.linkshorter.util.ApplicationProperties;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties({ ApplicationProperties.class })
+@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
 public class LinkshorterApplication {
 
 	public static void main(String[] args) {
