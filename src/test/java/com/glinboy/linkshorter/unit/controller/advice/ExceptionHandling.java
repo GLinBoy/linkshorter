c061bb5596a8d44ce8c3473c6123b8482e0abfd6
GLinBoy <hojjatabedie@gmail.com>
2023-04-09 20:16:13 +0200

    👌 IMPROVE: Removed Zalando Problem dependency

--- src/test/java/com/glinboy/linkshorter/unit/controller/advice/ExceptionHandling.java
+++ src/test/java/com/glinboy/linkshorter/unit/controller/advice/ExceptionHandling.java
@@ -1,9 +1,8 @@
 package com.glinboy.linkshorter.unit.controller.advice;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.zalando.problem.spring.web.advice.ProblemHandling;
 
 @ControllerAdvice
-final class ExceptionHandling implements ProblemHandling {
+final class ExceptionHandling {
 
 }
