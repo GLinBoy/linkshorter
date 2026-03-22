43ad8a6bcc1f004f708843ad8462e82aebce62c5
GLinBoy <glinboy@live.com>
2026-03-22 11:19:19 +0100

    👌 IMPROVE: Upgrade Spring Boot to 4.0.4
    resolved #195

--- src/main/java/com/glinboy/linkshorter/LinkshorterApplication.java
+++ src/main/java/com/glinboy/linkshorter/LinkshorterApplication.java
@@ -3,8 +3,8 @@ package com.glinboy.linkshorter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.glinboy.linkshorter.util.ApplicationProperties;
